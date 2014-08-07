a2d9061673c5fb2c2cf894aa02045e7afec976c6
joneshf <jones3.hardy@gmail.com>
2014-08-07 13:11:30 -0700

    Use variadic functions for callbacks.

--- README.md
+++ README.md
@@ -14,22 +14,53 @@
 
     class EventEmitter e where
 
+    class Variadic func ret where
+
+
+### Type Class Instances
+
+    instance variadicFn0 :: Variadic (Fn0 a) a
+
+    instance variadicFn1 :: Variadic (Fn1 a b) b
+
+    instance variadicFn10 :: Variadic (Fn10 a b c d e f g h i j k) k
+
+    instance variadicFn2 :: Variadic (Fn2 a b c) c
+
+    instance variadicFn3 :: Variadic (Fn3 a b c d) d
+
+    instance variadicFn4 :: Variadic (Fn4 a b c d e) e
+
+    instance variadicFn5 :: Variadic (Fn5 a b c d e f) f
+
+    instance variadicFn6 :: Variadic (Fn6 a b c d e f g) g
+
+    instance variadicFn7 :: Variadic (Fn7 a b c d e f g h) h
+
+    instance variadicFn8 :: Variadic (Fn8 a b c d e f g h i) i
+
+    instance variadicFn9 :: Variadic (Fn9 a b c d e f g h i j) j
+
 
 ### Values
 
-    addListener :: forall eff eff' e a. (EventEmitter e) => Event -> Eff eff a -> e -> Eff (event :: EventEff | eff') e
+    addListener :: forall eff e fn. (EventEmitter e, Variadic fn (Eff eff Unit)) => Event -> fn -> e -> Eff (event :: EventEff | eff) e
 
-    listeners :: forall eff eff' e a. (EventEmitter e) => Event -> e -> Eff (event :: EventEff | eff') [Event]
+    emitterHelper1 :: forall eff e a b. (EventEmitter e) => String -> a -> e -> Eff (event :: EventEff | eff) b
 
-    on :: forall eff eff' e a. (EventEmitter e) => Event -> Eff eff a -> e -> Eff (event :: EventEff | eff') e
+    emitterHelper2 :: forall eff e fn. (EventEmitter e, Variadic fn (Eff eff Unit)) => String -> Event -> fn -> e -> Eff (event :: EventEff | eff) e
 
-    once :: forall eff eff' e a. (EventEmitter e) => Event -> Eff eff a -> e -> Eff (event :: EventEff | eff') e
+    listeners :: forall eff e. (EventEmitter e) => Event -> e -> Eff (event :: EventEff | eff) [Event]
 
-    removeAllListeners :: forall eff eff' e a. (EventEmitter e) => [Event] -> e -> Eff (event :: EventEff | eff') e
+    on :: forall eff e fn. (EventEmitter e, Variadic fn (Eff eff Unit)) => Event -> fn -> e -> Eff (event :: EventEff | eff) e
 
-    removeListener :: forall eff eff' e a. (EventEmitter e) => Event -> Eff eff a -> e -> Eff (event :: EventEff | eff') e
+    once :: forall eff e fn. (EventEmitter e, Variadic fn (Eff eff Unit)) => Event -> fn -> e -> Eff (event :: EventEff | eff) e
 
-    setMaxListeners :: forall eff eff' e a. (EventEmitter e) => Number -> e -> Eff (event :: EventEff | eff') e
+    removeAllListeners :: forall eff e. (EventEmitter e) => [Event] -> e -> Eff (event :: EventEff | eff) e
+
+    removeListener :: forall eff e fn. (EventEmitter e, Variadic fn (Eff eff Unit)) => Event -> fn -> e -> Eff (event :: EventEff | eff) e
+
+    setMaxListeners :: forall eff e. (EventEmitter e) => Number -> e -> Eff (event :: EventEff | eff) e
 
 
 
--- src/Node/Events.purs
+++ src/Node/Events.purs
@@ -1,5 +1,5 @@
 module Node.Events
-  ( Event(..)
+  {-( Event(..)
   , EventEff()
   , EventEmitter
   , addListener
@@ -9,13 +9,41 @@ module Node.Events
   , removeAllListeners
   , removeListener
   , setMaxListeners
-  ) where
+  ) -}where
 
   import Control.Monad.Eff (returnE, Eff())
+
+  import Data.Function
+    ( Fn0()
+    , Fn1()
+    , Fn2()
+    , Fn3()
+    , Fn4()
+    , Fn5()
+    , Fn6()
+    , Fn7()
+    , Fn8()
+    , Fn9()
+    , Fn10()
+    )
 
   foreign import data EventEff :: !
 
   class EventEmitter e
+
+  class Variadic func ret
+
+  instance variadicFn0  :: Variadic (Fn0  a) a
+  instance variadicFn1  :: Variadic (Fn1  a b) b
+  instance variadicFn2  :: Variadic (Fn2  a b c) c
+  instance variadicFn3  :: Variadic (Fn3  a b c d) d
+  instance variadicFn4  :: Variadic (Fn4  a b c d e) e
+  instance variadicFn5  :: Variadic (Fn5  a b c d e f) f
+  instance variadicFn6  :: Variadic (Fn6  a b c d e f g) g
+  instance variadicFn7  :: Variadic (Fn7  a b c d e f g h) h
+  instance variadicFn8  :: Variadic (Fn8  a b c d e f g h i) i
+  instance variadicFn9  :: Variadic (Fn9  a b c d e f g h i j) j
+  instance variadicFn10 :: Variadic (Fn10 a b c d e f g h i j k) k
 
   newtype Event = Event String
 
@@ -31,12 +59,12 @@ module Node.Events
     \      }\
     \    }\
     \  }\
-    \}" :: forall eff eff' e a b c
+    \}" :: forall eff e a b
         .  (EventEmitter e)
         => String
-        -> b
+        -> a
         -> e
-        -> Eff (event :: EventEff | eff') c
+        -> Eff (event :: EventEff | eff) b
 
   foreign import emitterHelper2
     "function emitterHelper2(__dict) {\
@@ -49,38 +77,38 @@ module Node.Events
     \      }\
     \    }\
     \  }\
-    \}" :: forall eff eff' e a
-        .  (EventEmitter e)
+    \}" :: forall eff e fn
+        .  (EventEmitter e, Variadic fn (Eff eff Unit))
         => String
         -> Event
-        -> Eff eff a
+        -> fn
         -> e
-        -> Eff (event :: EventEff | eff') e
+        -> Eff (event :: EventEff | eff) e
 
-  addListener :: forall eff eff' e a. (EventEmitter e)
-              => Event -> Eff eff a -> e -> Eff (event :: EventEff | eff') e
+  addListener :: forall eff e fn. (EventEmitter e, Variadic fn (Eff eff Unit))
+              => Event -> fn -> e -> Eff (event :: EventEff | eff) e
   addListener = emitterHelper2 "addListener"
 
-  on :: forall eff eff' e a. (EventEmitter e)
-     => Event -> Eff eff a -> e -> Eff (event :: EventEff | eff') e
+  on :: forall eff e fn. (EventEmitter e, Variadic fn (Eff eff Unit))
+     => Event -> fn -> e -> Eff (event :: EventEff | eff) e
   on = emitterHelper2 "on"
 
-  once :: forall eff eff' e a. (EventEmitter e)
-       => Event -> Eff eff a -> e -> Eff (event :: EventEff | eff') e
+  once :: forall eff e fn. (EventEmitter e, Variadic fn (Eff eff Unit))
+       => Event -> fn -> e -> Eff (event :: EventEff | eff) e
   once = emitterHelper2 "once"
 
-  removeListener :: forall eff eff' e a. (EventEmitter e)
-                 => Event -> Eff eff a -> e -> Eff (event :: EventEff | eff') e
+  removeListener :: forall eff e fn. (EventEmitter e, Variadic fn (Eff eff Unit))
+                 => Event -> fn -> e -> Eff (event :: EventEff | eff) e
   removeListener = emitterHelper2 "removeListener"
 
-  removeAllListeners :: forall eff eff' e a.  (EventEmitter e)
-                     => [Event] -> e -> Eff (event :: EventEff | eff') e
+  removeAllListeners :: forall eff e. (EventEmitter e)
+                     => [Event] -> e -> Eff (event :: EventEff | eff) e
   removeAllListeners = emitterHelper1 "removeAllListeners"
 
-  setMaxListeners :: forall eff eff' e a.  (EventEmitter e)
-                  => Number -> e -> Eff (event :: EventEff | eff') e
+  setMaxListeners :: forall eff e. (EventEmitter e)
+                  => Number -> e -> Eff (event :: EventEff | eff) e
   setMaxListeners = emitterHelper1 "setMaxListeners"
 
-  listeners :: forall eff eff' e a.  (EventEmitter e)
-            => Event -> e -> Eff (event :: EventEff | eff') [Event]
+  listeners :: forall eff e. (EventEmitter e)
+            => Event -> e -> Eff (event :: EventEff | eff) [Event]
   listeners = emitterHelper1 "listeners"
