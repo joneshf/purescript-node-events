70accceb0c272efff30c4b57ab78daa38a665a96
joneshf <jones3.hardy@gmail.com>
2014-10-06 14:05:14 -0700

    Use more external stuff.

--- README.md
+++ README.md
@@ -7,40 +7,9 @@
     data Emitter :: *
 
 
-### Type Classes
-
-    class EventEmitter e where
-
-    class Variadic func ret where
-
-
 ### Type Class Instances
 
     instance eventEmitterEmitter :: EventEmitter Emitter
-
-    instance variadicArr :: Variadic (a -> b) c
-
-    instance variadicFn0 :: Variadic (Fn0 a) b
-
-    instance variadicFn1 :: Variadic (Fn1 a b) c
-
-    instance variadicFn10 :: Variadic (Fn10 a b c d e f g h i j k) l
-
-    instance variadicFn2 :: Variadic (Fn2 a b c) d
-
-    instance variadicFn3 :: Variadic (Fn3 a b c d) e
-
-    instance variadicFn4 :: Variadic (Fn4 a b c d e) f
-
-    instance variadicFn5 :: Variadic (Fn5 a b c d e f) g
-
-    instance variadicFn6 :: Variadic (Fn6 a b c d e f g) h
-
-    instance variadicFn7 :: Variadic (Fn7 a b c d e f g h) i
-
-    instance variadicFn8 :: Variadic (Fn8 a b c d e f g h i) j
-
-    instance variadicFn9 :: Variadic (Fn9 a b c d e f g h i j) k
 
 
 ### Values
--- src/Node/Events.purs
+++ src/Node/Events.purs
@@ -1,7 +1,5 @@
 module Node.Events
   ( Emitter()
-  , EventEmitter
-  , Variadic
   -- Creating listeners.
   , emitter
   -- Manipulating listeners.
@@ -19,41 +17,10 @@ module Node.Events
   , removeListenerEvent
   ) where
 
-  import Control.Events (Event(..), EventEff())
+  import Control.Events (Event(..), EventEff(), EventEmitter, Variadic)
   import Control.Monad.Eff (returnE, Eff())
 
-  import Data.Function
-    ( Fn0()
-    , Fn1()
-    , Fn2()
-    , Fn3()
-    , Fn4()
-    , Fn5()
-    , Fn6()
-    , Fn7()
-    , Fn8()
-    , Fn9()
-    , Fn10()
-    )
-
-  class EventEmitter e
-
   instance eventEmitterEmitter :: EventEmitter Emitter
-
-  class Variadic func ret
-
-  instance variadicArr  :: Variadic (a -> b) c
-  instance variadicFn0  :: Variadic (Fn0  a) b
-  instance variadicFn1  :: Variadic (Fn1  a b) c
-  instance variadicFn2  :: Variadic (Fn2  a b c) d
-  instance variadicFn3  :: Variadic (Fn3  a b c d) e
-  instance variadicFn4  :: Variadic (Fn4  a b c d e) f
-  instance variadicFn5  :: Variadic (Fn5  a b c d e f) g
-  instance variadicFn6  :: Variadic (Fn6  a b c d e f g) h
-  instance variadicFn7  :: Variadic (Fn7  a b c d e f g h) i
-  instance variadicFn8  :: Variadic (Fn8  a b c d e f g h i) j
-  instance variadicFn9  :: Variadic (Fn9  a b c d e f g h i j) k
-  instance variadicFn10 :: Variadic (Fn10 a b c d e f g h i j k) l
 
   newListenerEvent :: Event
   newListenerEvent = Event "newListener"
