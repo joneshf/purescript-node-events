39763e7a062e4393f31d52128cf1d800b6f46ae3
Simon Van Casteren <simon.van.casteren@gmail.com>
2015-10-22 18:36:15 +0200

    Updated to compiler version 0.7.5.0

--- test/Node/Events.purs
+++ test/Node/Events.purs
@@ -1,18 +1,17 @@
 module Test.Node.Events where
 
+  import Prelude
   import Control.Monad.Eff (Eff())
 
-  import Debug.Trace (print, Trace())
+  import Control.Monad.Eff.Console (log, CONSOLE())
 
-  import Node.Events (Event(..), EventEff(), emit, emitter, on, newListenerEvent)
+  import Control.Events(Event(..), EventEff())
+  import Node.Events (emit, emitter, on, newListenerEvent)
 
-  foreign import toString
-    "function toString(x) {\
-    \  return x.toString();\
-    \};" :: forall a. a -> String
+  foreign import toString :: forall a. a -> String
 
-  main :: Eff (event :: EventEff, trace :: Trace) Boolean
+  main :: Eff (event :: EventEff, console :: CONSOLE) Boolean
   main = emitter
-    >>= on newListenerEvent (\name -> print $ "added a new event: " ++ toString name)
-    >>= on (Event "Here's some candy") (\candy -> print $ "Check out this candy: " ++ toString candy)
+    >>= on newListenerEvent (\name -> log $ "added a new event: " ++ toString name)
+    >>= on (Event "Here's some candy") (\candy -> log $ "Check out this candy: " ++ toString candy)
     >>= emit (Event "Here's some candy") "Skittles"
